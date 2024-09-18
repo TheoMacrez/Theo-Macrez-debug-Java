955a2aed5c814ac10ea366e9f1ea7a5116b41e59
TheoMacrez <35222332+theomacrezAM@users.noreply.github.com>
2024-09-18 12:41:34 +0200

    Refactor2 AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,9 +6,7 @@ import java.io.FileWriter;
 import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
+
 
 	private ISymptomReader symptomReader;
 	private ISymptomWriter symptomWriter;
@@ -16,39 +14,6 @@ public class AnalyticsCounter {
 	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
 		symptomReader = reader;
 		symptomWriter = writer;
-	}
-	
-	public static void main(String args[]) throws Exception {
-
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
 	}
 
 	public List<String> getSymptoms() {
