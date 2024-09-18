c99bd7dfb7376664a74975268c20d63cdf8695c9
TheoMacrez <35222332+theomacrezAM@users.noreply.github.com>
2024-09-18 12:40:01 +0200

    Refactor Analytics Counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,13 +3,23 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
+
+	private ISymptomReader symptomReader;
+	private ISymptomWriter symptomWriter;
+
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		symptomReader = reader;
+		symptomWriter = writer;
+	}
 	
 	public static void main(String args[]) throws Exception {
+
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
@@ -41,5 +51,28 @@ public class AnalyticsCounter {
 		writer.close();
 	}
 
+	public List<String> getSymptoms() {
+		return symptomReader.GetSymptoms();
+	}
+
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String,Integer> newSymptomsMap = new HashMap<>();
+		for(int i =0; i <symptoms.size();i++)
+		{
+			newSymptomsMap.putIfAbsent(symptoms.get(i),0);
+			int toAdd = newSymptomsMap.get(symptoms.get(i))+1;
+			newSymptomsMap.replace(symptoms.get(i),toAdd);
+
+		}
+		return newSymptomsMap;
+	}
+
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		return new TreeMap<>(symptoms);
+	}
+
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		symptomWriter.writeSymptoms(symptoms);
+	}
 
 }
